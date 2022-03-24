8b34f170d34cc3511a9a6ad7cddf9dba35155c6d
Julian Barco <julianochoa1999@gmail.com>
2022-03-24 14:46:02 -0500

    form Create Appointment

--- src/pages/CreateAppointment.js
+++ src/pages/CreateAppointment.js
@@ -2,8 +2,27 @@ import React from "react";
 
 const CreateAppointment = () => {
   return (
-    <div style={{ minHeight: "600px", marginTop: "6rem" }}>
-      CreateAppointment
+    <div style={{ minHeight: "600px", marginTop: "6rem" }} class="container ">
+
+      <h1>Crear cita</h1>
+      <form action=''>
+        <label for="name" class="form-label">Nombre de coder</label>
+        <input type="text" class="form-control" id="name"></input>
+
+        <label for="name" class="form-label">Tema de consulta</label>
+        <textarea class="form-control" id="tema" ></textarea>
+
+      
+        <label for="date" class="form-label">Fecha consulta</label>
+        <br/>
+        <input class="" type="date"></input>
+        <br/>
+        <label for="date" class="form-label">Hora consulta</label>
+        <br/>
+        <input class="" type="time"></input>
+        <br/>
+        <button type="submit" class="btn btn-primary mt-3">Crear</button>
+      </form>
     </div>
   );
 };
