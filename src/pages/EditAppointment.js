bf3da38f6e1aa8c891a36ab20e4a90c7492d1ccc
Julian Barco <julianochoa1999@gmail.com>
2022-03-25 10:54:56 -0500

    Funtion handleChange

--- src/pages/EditAppointment.js
+++ src/pages/EditAppointment.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from 'react';
 
 const EditAppointment = () => {
+
+
+  const[form,setForm] = useState({});
+  const handleChange = e =>{
+    setForm({
+      ...form,
+      [e.target.name]:e.target.value,
+    });
+  };
+
+  
+  
+
   return (
+
+
     <div style={{ minHeight: "100vh", marginTop: "6rem", width: "20rem" }} class="d-flex mx-auto">
 
       <div class=" container border border-2 border-secondary rounded-3 mx-auto 
@@ -11,33 +26,55 @@ const EditAppointment = () => {
           <form action=''>
             <div class="row mb-3">
               <div class="col">
-                <label for="name" class="form-label">Nombre de coder</label>
-                <input type="text" class="form-control" id="name"></input>
+                <label htmlFor="name" class="form-label">Nombre de coder</label>
+
+                <input 
+                type="text" 
+                class="form-control"
+                id="name"
+                name="nombre"
+                value={form.nombre}
+                onChange={handleChange}
+                />
+                
               </div>
             </div>
 
             <div class="row mb-3">
               <div class="col">
-                <label for="name" class="form-label">Tema de consulta</label>
-                <textarea class="form-control" id="tema" ></textarea>
+                <label htmlFor="tema" class="form-label">Tema de consulta</label>
+                <textarea class="form-control"
+                id="tema" 
+                name="tema"
+                value={form.tema}
+                onChange={handleChange}
+                />
               </div>
             </div>
 
 
             <div class="row mb-3">
               <div class="col">
-                <label for="date" class="form-label">Fecha consulta</label>
+                <label htmlFor="date" class="form-label">Fecha consulta</label>
                 <br />
-                <input class="" type="date"></input>
+                <input class="" type="date"
+                name='date'
+                value={form.date}
+                onChange={handleChange}
+                />
                 <br />
               </div>
             </div>
 
             <div class="row mb-3">
               <div class="col">
-                <label for="date" class="form-label">Hora consulta</label>
+                <label htmlFor="date" class="form-label">Hora consulta</label>
                 <br />
-                <input class="" type="time"></input>
+                <input class="" type="time"
+                name='time'
+                value={form.time}
+                onChange={handleChange}
+                />
                 <br />
               </div>
             </div>
